aae2990d1e87f07d98dbe6957d68b537fcb6d3c0
hawa <ragab5434@gmail.com>
2022-12-17 12:07:50 +0300

    some edit in front-end

--- src/project/urls.py
+++ src/project/urls.py
@@ -20,7 +20,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('movies/', include('movie.urls', namespace='movies')),
+    path('', include('movie.urls', namespace='movies')),
     path('about/', include('about.urls', namespace='about')),
 ]
 
